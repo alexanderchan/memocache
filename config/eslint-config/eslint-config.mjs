a1004b495ddbd5751c57fe2057bf0c415717d9bf
Alex Chan <alexander.m.chan@gmail.com>
2024-10-13 09:57:50 -0600

    common lint

---
+++ config/eslint-config/eslint-config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  //   ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    ignores: ['dist/'],
+  },
+)
